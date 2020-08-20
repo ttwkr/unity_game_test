3fcff0feea3113ffb84a6d015c5b0d5d57d00ff5
ttwkr <ttwkor12@gmail.com>
2020-08-21 01:48:38 +0900

    방향키 입력 오브젝트 이동 연습

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -20,6 +20,7 @@ namespace DefaultNamespace
         private void Start()
         {
             Debug.Log("사냥할 준비가 됐습니다.");
+
         }
         //--------------------------------------------
         //        이 윗 부분이 초기화 하는 부분이라고 생각하면 된다
@@ -29,7 +30,7 @@ namespace DefaultNamespace
         {
             //이부분은 계속 고정적을 CPU를 많이 사용한다
             // 물리 연산 하는 곳이다.
-            Debug.Log("이동!");
+            // Debug.Log("이동!");
         }
 
         private void Update()
@@ -37,13 +38,53 @@ namespace DefaultNamespace
             //게임 로직 업데이트
             // 주기적으로 돌아가는 로직을 수행하는곳
             // 환경에 따라서 실행주기가 떨어질수 있다.
-            Debug.Log("몬스터 사냥!!");
+            Debug.Log("업데이트함수가 실행중");
+            // if (Input.anyKeyDown)
+            // {
+            //     Debug.Log("키를 눌렀습니다.");
+            // }
+            
+            
+            //int number = 4; //스칼라 값
+            Vector3 vec = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")); //벡터값
+            //Translate --> 벡터값을 현재위치에 더하는 함수 
+            transform.Translate(vec);
+
+            // if (Input.GetKeyDown(KeyCode.Return))
+            // {
+            //     Debug.Log( KeyCode.Return.ToString()+" 키를 눌렀습니다.");
+            // }
+            //
+            // if (Input.GetKey(KeyCode.RightArrow))
+            // {
+            //     Debug.Log(KeyCode.RightArrow.ToString() + " 키를 누르고 있습니다.");
+            // }
+            //
+            // if (Input.GetKeyUp(KeyCode.LeftArrow))
+            // {
+            //     Debug.Log(KeyCode.LeftArrow.ToString() + " 키를 뗐습니다.");
+            // }
+            // Input Manager에 있는 값을 불러와서 컨트롤 한다.
+            // if (Input.GetButton("Jump"))
+            // {
+            //     Debug.Log("점프 모으는중....");
+            // }
+            //
+            // if (Input.GetButtonDown("Jump"))
+            // {
+            //     Debug.Log("점프!");
+            // }
+            //
+            // if (Input.GetButtonUp("Jump"))
+            // {
+            //     Debug.Log("겁나 높게 점프!!");
+            // }
         }
 
         private void LateUpdate()
         {
             //모든 업데이트가 다 끝난후 마지막에 실행
-            Debug.Log("경험치 획득");
+            Debug.Log("LateUpdate가 실행중");
         }
 
         private void OnDisable()
